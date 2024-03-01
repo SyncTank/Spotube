81491fcf373ca84399477eba8f0228062a2841d4
Rudy <30980904+SyncTank@users.noreply.github.com>
2024-02-29 19:18:36 -0500

    New PR

---
+++ UserAuth.py
@@ -0,0 +1,10 @@
+
+class Authentication:
+    def __init__(self):
+        self.client_id = 'YOUR_CLIENT_ID'
+        self.client_secret = 'Your_SECRET_KEY'
+        self.authenticate()
+
+    def authenticate(self):
+        self.client_id = ''
+        self.client_secret = ''
