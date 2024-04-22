62152897c841b5cf5f83655dd5dc1d58b213cb08
Rudy <30980904+SyncTank@users.noreply.github.com>
2024-04-22 00:24:31 -0400

    added test playlists

--- src/main.py
+++ src/main.py
@@ -8,6 +8,17 @@ import asyncio
 # New_user = App_authentication()
 # New_user.request_example()
 
+# https://open.spotify.com/playlist/6ykxrtWbLE6KzU8ISQ6xqM?si=ad4e308e24cf4381
+
+# https://youtube.com/playlist?list=PLaQ2znw11EHJ0KlUDDzuXyqll-db3hWX6&si=WEOSDl4QMg2JlcW4
+
+async def main():
+    print('Hello ...')
+    await asyncio.sleep(1)
+    print('... World!')
+
+asyncio.run(main())
+
 def main():
     main_window = MainWindow(800, 600)
 
