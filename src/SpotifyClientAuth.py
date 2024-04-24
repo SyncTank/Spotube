31aaad9080be23eaf30f553f1dd192dc11e6e34a
Rudy <30980904+SyncTank@users.noreply.github.com>
2024-04-24 10:01:21 -0400

    refactored variable

--- src/SpotifyClientAuth.py
+++ src/SpotifyClientAuth.py
@@ -2,51 +2,51 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 
-class Spotify_Auth:
+class SpotifyAuth:
     def __init__(self):
-        self._client_id: str = 'YOUR_CLIENT_ID'
-        self._client_secret: str = 'Your_SECRET_KEY'
+        self.__client_id: str = 'YOUR_CLIENT_ID'
+        self.__client_secret: str = 'Your_SECRET_KEY'
         self._content: list = []
-        self._base_url: str = 'https://api.spotify.com/v1/'
-        self._response: str = ''
+        self.__base_url: str = 'https://api.spotify.com/v1/'
+        self.__url_token: str = "https://accounts.spotify.com/api/token"
+        self.response: str = ''
         self.__token: str = ''
-        self.refresh_token: str = ''
         self._setup_client()
         self._authenticate()
 
     def _setup_client(self) -> None:
         try:
-            with open("../private/SpotifyClientAuth.txt", "r") as file:
+            with open("/private/SpotifyClientAuth.txt", "r") as file:
                 self._content = file.readlines()
         except FileNotFoundError:
             print("File does not exist.")
 
         if self._content is not None:
-            self._client_id = self._content[0].replace("\n", "")
-            self._client_secret = self._content[1]
+            self.__client_id = self._content[0].replace("\n", "")
+            self.__client_secret = self._content[1]
         else:
             print("Please read docs or contact the administrator.")
 
     def _authenticate(self, status=200) -> None:
         if status == 200:
             # Authenticate with Spotify API
-            self._response = requests.post('https://accounts.spotify.com/api/token',
+            self.response = requests.post(self.__url_token,
                                           data={'grant_type': 'client_credentials'},
-                                          auth=HTTPBasicAuth(self._client_id, self._client_secret))
+                                          auth=HTTPBasicAuth(self.__client_id, self.__client_secret))
             # Extract access token from _response
-            self.__token = self._response.json().get('access_token')
+            self.__token = self.response.json().get('access_token')
         else:
             # Refresh _response to get a new token - Testing
-            self._response = requests.post('https://accounts.spotify.com/api/token',
+            self.response = requests.post(self.__url_token,
                                           data={'grant_type': 'refresh_token',
-                                                'refresh_token': self.__token,},
-                                          auth=HTTPBasicAuth(self._client_id, self._client_secret))
+                                                'refresh_token': self.__token, },
+                                          auth=HTTPBasicAuth(self.__client_id, self.__client_secret))
 
-            self.__token = self._response.json().get('access_token')
+            self.__token = self.response.json().get('access_token')
 
-
-    def request_example(self) -> None:
+    def request_example(self, request: str) -> None:
+        request = "artists/4Z8W4fKeB5YxbusRsdQVPb"
         headers = {"Authorization": f"Bearer {self.__token}", }
-        _response = requests.get("https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb", headers=headers)
+        response = requests.get(f"{self.__base_url}{request}", headers=headers)
 
-        print(_response.json(), _response.status_code)
+        print(response.json(), response.status_code)
