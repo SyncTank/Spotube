cef3f5af6f855d9d39fcdb327b2cfcc04e760687
Rudy <30980904+SyncTank@users.noreply.github.com>
2024-04-24 11:54:38 -0400

    clean ups

--- src/InterfaceSpt.py
+++ src/InterfaceSpt.py
@@ -1,6 +1,16 @@
 import dearpygui.dearpygui as dpg
 
 
+def save_callback():
+    input_text = dpg.get_value("string")
+    print(f"Save Clicked {input_text}")
+
+
+def save_callback_two():
+    input_text = dpg.get_value("string2")
+    print(f"Save Clicked {input_text}")
+
+
 class MainWindow:
     def __init__(self, width, height):
         self.__width = width
@@ -8,24 +18,16 @@ class MainWindow:
         self.__title = "Spotube"
         self.setup()
 
-    def save_callback(self):
-        input_text = dpg.get_value("string")
-        print(f"Save Clicked {input_text}")
-
-    def save_callback_two(self):
-        input_text = dpg.get_value("string2")
-        print(f"Save Clicked {input_text}")
-
     def setup(self):
         dpg.create_context()
 
         with dpg.window(tag="Primary Window"):
             dpg.add_text(default_value="Spotify Client", label="Spotify Client")
             dpg.add_input_text(label="Path", hint="Quick brown fox", tag="string")
-            dpg.add_button(label="Save", callback=self.save_callback)
+            dpg.add_button(label="Save", callback=save_callback)
             dpg.add_text(default_value="Youtube Client", label="Youtube Client")
             dpg.add_input_text(label="Path", hint="Quick brown fox")
-            dpg.add_button(label="Save", callback=self.save_callback_two)
+            dpg.add_button(label="Save", callback=save_callback_two)
             dpg.add_text(default_value="Output", label="Output")
             dpg.add_input_text(label="Path", hint="Quick brown fox")
 
