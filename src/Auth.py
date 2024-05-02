59dc8cabd57da1a3739e125cb61247f957a9165f
Rudy <30980904+SyncTank@users.noreply.github.com>
2024-05-01 21:23:30 -0400

    base method to inheret

---
+++ src/Auth.py
@@ -0,0 +1,46 @@
+class Auth:
+    def __init__(self):
+        self.__client_id: str = 'YOUR_CLIENT_ID'
+        self.__client_secret: str = 'Your_SECRET_KEY'
+        self.content: list = []
+        self.__base_url: str = ''
+        self.__url_token: str = ""
+        self.response: str = ''
+        self.__token: str = ''
+        self.__setup_client()
+        try:
+            self.__authenticate()
+        except Exception as e:
+            print(e)
+
+    def __setup_client(self, file_path: str) -> None:
+        try:
+            with open(f"private/{file_path}", "r") as file:
+                self.content = file.readlines()
+        except FileNotFoundError:
+            print("File does not exist.")
+
+        if self.content is not None:
+            self.__client_id = self.content[0].replace("\n", "")
+            self.__client_secret = self.content[1]
+        else:
+            print("Please read docs or contact the administrator.")
+        self.content.clear()
+
+    def __authenticate(self, status=200) -> None:
+        if status == 200 and len(self.__token) < 1:
+            self.response = requests.post(self.__url_token,
+                                          data={'grant_type': 'client_credentials'},
+                                          auth=HTTPBasicAuth(self.__client_id, self.__client_secret))
+            self.__token = self.response.json().get('access_token')
+        elif status != 200:
+            self.response = requests.post(self.__url_token,
+                                          data={'grant_type': 'refresh_token',
+                                                'refresh_token': self.__token, },
+                                          auth=HTTPBasicAuth(self.__client_id, self.__client_secret))
+
+            self.__token = self.response.json().get('access_token')
+        else:
+            raise Exception("Invalid token.")
+
+        print(f"Access Token: {self.__token}")
