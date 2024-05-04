3eb5a124c974b80ca2ae15b09216e906e0c6fcf5
Rudy <30980904+SyncTank@users.noreply.github.com>
2024-05-03 20:53:01 -0400

    base method to inheret

--- src/YoutubeClientAuth.py
+++ src/YoutubeClientAuth.py
@@ -12,7 +12,7 @@ def youtube_relog() -> list:
 
     print(type(file))
     for i, v in enumerate(file):
-        print(i, v)
+        print(i, f"{v}")
 
     return request_data
 
@@ -27,14 +27,14 @@ class YoutubeAuth:
             os.remove(self.playlist_name)
 
         playlist = Playlist(request)
-        playlist_data = set()
+        playlist_data = []
         for url in playlist.video_urls:
             youtube = YouTube(url)
-            playlist_data.add(youtube.title)
+            playlist_data.append(youtube.title)
             print(f"Title: {youtube.title}")
 
         with open("YoutubeDump.txt", "w") as file:
-            file.write(str(playlist_data))
+            file.writelines(playlist_data)
 
     def create_playlist(self, request: str) -> NotImplemented:
         return NotImplemented
