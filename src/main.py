90804b6b69d53d801a1906de678a1283529ab58c
Rudy <30980904+SyncTank@users.noreply.github.com>
2024-05-18 18:31:57 -0400

    added flask for user auth

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+import os
+
 from Auth import set_path, Auth
 import time
-import spotipy.oauth2 as SpotifyOAuth
+from spotipy.oauth2 import SpotifyOAuth
 from flask import Flask, request, url_for, session, redirect
 from SpotifyClientAuth import SpotifyAuth, spotify_relog
 from YoutubeClientAuth import YoutubeAuth, youtube_relog
@@ -24,57 +26,11 @@ from InterfaceSpt import MainWindow
 # https://open.spotify.com/playlist/4ChHbaRX9II5dHCKrPkgWP?si=03c68fd95cbe4833
 
 app = Flask(__name__)
-app.config['SESSION_COOKIE_NAME'] = 'Spotify Cookie'
-app.secret_key = 'secret key'
-TOKEN_INFO = 'token_info'
-
-
-@app.route('/')
-def login():
-    auth_url = create_spotify_oauth().get_authorize_url()
-    return redirect(auth_url)
-
-
-@app.route('/redirect')
-def redirect_page():
-    session.clear()
-    code = request.args.get('code')
-    token_info = create_spotify_oauth().get_access_token(code)
-    session[TOKEN_INFO] = token_info
-    return redirect(url_for('login', _external=True))
-
-
-def get_token():
-    token_info = session.get(TOKEN_INFO, None)
-    if not token_info:
-        # if the token info is not found, redirect the user to the login route
-        redirect(url_for('login', _external=False))
-
-    # check if the token is expired and refresh it if necessary
-    now = int(time.time())
-
-    is_expired = token_info['expires_at'] - now < 60
-    if is_expired:
-        spotify_oauth = create_spotify_oauth()
-        token_info = spotify_oauth.refresh_access_token(token_info['refresh_token'])
-
-    return token_info
-
-
-def create_spotify_oauth():
-    return SpotifyOAuth(
-        client_id='YOUR_CLIENT_ID',
-        client_secret='YOUR_CLIENT_SECRET',
-        redirect_uri=url_for('redirect_page', _external=True),
-        scope='user-library-read playlist-modify-public playlist-modify-private'
-    )
-
-
-app.run(debug=True)
 
 
 def main():
     set_path()
+
 
     #spotify_client = SpotifyAuth()
     #youtube_client = YoutubeAuth()
